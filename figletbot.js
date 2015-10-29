37afa91114266a85dd5b0f26ce1045d6e0c33aab
Ivan Bojer <ijb@networkrift.com>
2015-10-29 15:44:30 +0900

    improvements to figletbot;

--- figletbot.js
+++ figletbot.js
@@ -2,22 +2,33 @@ var figlet = require('figlet');
 
 module.exports = function (req, res, next) {
   var text = req.body.text;
+  var result = text.split(":");
+  txt = result[1];
+
+  if (txt == '') {
+    txt = result;
+  };
+
   var botPayload = {
     text : '!'
   };
 
-  figlet(text, function(err, data) {
+  figlet(txt, {
+    horizontalLayout: 'fitted',
+    verticalLayout: 'fitted'
+  },
+  function(err, data) {
     if (err) {
-        txt = err;
         console.log('Something went wrong...');
         console.dir(err);
         return;
-    }
+    };
+
     // write response message and add to payload
     botPayload.text = data;
 
-    console.log('txt is...');
-    console.log(botPayload);
+    //console.log('txt is...');
+    //console.log(botPayload);
   
 
     return res.status(200).json(botPayload);
